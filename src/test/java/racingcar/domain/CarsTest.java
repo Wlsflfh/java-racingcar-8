3f04c9d98ba1abbdcd9c42a62ada3ed1128fe713
Jinriro <riro12.hanyang.ac.kr>
2025-10-24 13:25:13 +0900

    test: 자동차 이름 중복에 대한 테스트 추가

--- src/test/java/racingcar/domain/CarsTest.java
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -1,4 +1,34 @@
-import static org.junit.jupiter.api.Assertions.*;
+package racingcar.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 class CarsTest {
-  
+
+    @Test
+    @DisplayName("정상적인 자동차 이름들 텍스트에 대하여 정상적으로 처리하는지 확인한다.")
+    void carsTest() {
+        // given
+        List<String> normalCarNames = List.of("tesla", "kia", "benz");
+
+        // when - then
+        assertDoesNotThrow(() -> {new Cars(normalCarNames);});
+    }
+
+    @Test
+    @DisplayName("중복된 자동차 이름들 텍스트에 대하여 예외를 발생시키는지 확인한다.")
+    void validateDuplicateTest() {
+        // given
+        List<String> duplicateCarNames = List.of("tesla", "tesla", "benz");
+
+        // when - then
+        assertThatThrownBy(() -> new Cars(duplicateCarNames))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름은 중복될 수 없습니다.");
+    }
 }
