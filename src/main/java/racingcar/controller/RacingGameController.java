1b55e3b8b1cfd4deb1ffbf0158f9ca52c937e443
Jinriro <riro12.hanyang.ac.kr>
2025-10-24 14:02:45 +0900

    feat: RacingGameController 구현

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -1,2 +1,29 @@
-package controller;public class GameController {
+package racingcar.controller;
+
+import racingcar.domain.Cars;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class RacingGameController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public RacingGameController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void play() {
+        Cars cars = new Cars(inputView.readCarNames());
+        int numberOfAttempts = inputView.readNumberOfAttempts();
+
+        outputView.printProgressHeader();
+        for (int i = 0; i < numberOfAttempts; i++) {
+            cars.moveCars();
+            outputView.printProgress(cars.getCars());
+        }
+
+        outputView.printWinner(cars.getWinner());
+    }
 }
