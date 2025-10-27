67761bb8a095d194ea85cdbe72a3b3d6f05f7142
Jinriro <riro12.hanyang.ac.kr>
2025-10-27 16:31:34 +0900

    chore: 줄간격 수정

--- src/main/java/racingcar/util/ErrorMessage.java
+++ src/main/java/racingcar/util/ErrorMessage.java
@@ -1,6 +1,7 @@
 package racingcar.util;
 
 public enum ErrorMessage {
+
     INVALID_INPUT("잘못된 입력입니다."),
     DO_NOT_DUPLICATE_CAR_NAME("자동차 이름은 중복될 수 없습니다."),
     MIN_CAR_NAME_LENGTH("자동차 이름은 1글자 이상이여야 합니다."),
